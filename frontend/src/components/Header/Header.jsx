980f92f7fe6bb9c02488a0d131624e448ab09386
MK <115914696+howtomake1billiondollars@users.noreply.github.com>
2024-12-19 13:22:14 +0700

    giao dien fe

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+ 
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Đặt món bạn thích tại đây</h2>
+        <p>Chọn từ menu</p>
+        <button>Xem menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
