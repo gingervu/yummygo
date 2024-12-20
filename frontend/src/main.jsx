4d8f6908a933ecb05d91dab2aa8cb525611a46c9
MK <115914696+howtomake1billiondollars@users.noreply.github.com>
2024-12-20 15:26:42 +0700

    change image and update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter} from 'react-router-dom'
 
+// ReactDOM ???
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  
     <App />
   </BrowserRouter>
     
 
 )
+
+
