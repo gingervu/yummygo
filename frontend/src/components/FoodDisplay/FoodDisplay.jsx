049ba669beecdf02a0f6361a27f5d3a1716926d7
MK <115914696+howtomake1billiondollars@users.noreply.github.com>
2024-12-20 13:20:35 +0700

    adding option

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,17 +1,27 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = ({category}) => {
-    const {food_list} = useContext(StoreContext)
-  return (
-    <div className='food-display' id='food-display'>
-        <h2>
-            Những món ăn/nhà hàng gần chỗ bạn
-        </h2>
-      
-    </div>
-  )
+const FoodDisplay = ({ category }) => {
+    const { food_list } = useContext(StoreContext)
+    return (
+        <div className='food-display' id='food-display'>
+            <h2>
+                Những món ăn/nhà hàng gần chỗ bạn
+            </h2>
+            <div className='food-display-list'>
+                {food_list.map((item, index) => {
+                    return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}
+                    />
+                }
+
+                )
+                }
+            </div>
+
+        </div>
+    )
 }
 
 export default FoodDisplay
