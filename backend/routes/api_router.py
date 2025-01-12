b4815fd5255d0920563cb1cbaa8b08a81633daf9
spicyginger <lalaphongtulinh@gmail.com>
2025-01-12 11:30:14 +0700

    update router

--- backend/routes/api_router.py
+++ backend/routes/api_router.py
@@ -5,13 +5,12 @@ from controllers.driver_controller import router as driver_router
 from controllers.manager_controller import router as manager_router
 from controllers.restaurant_controller import router as restaurant_router
 from controllers.menu_item_controller import router as menu_item_router
-from controllers.merchant_controller import router as merchant_router
 from controllers.order_controller import router as order_router
 from controllers.order_item_controller import router as order_item_router
 from controllers.restaurant_controller import router as restaurant_router
-from controllers.restaurant_times_controller import router as restaurant_times_router
 from controllers.user_controller import router as user_router
-
+from controllers.register_controller import router as register_router
+from controllers.login_controller import router as login_router
 # Tập hợp tất cả các router
 api_router = APIRouter()
 
@@ -21,8 +20,8 @@ api_router.include_router(admin_router)
 api_router.include_router(customer_router)
 api_router.include_router(driver_router)
 api_router.include_router(manager_router)
-api_router.include_router(merchant_router)
 api_router.include_router(restaurant_router)
 api_router.include_router(menu_item_router)
 api_router.include_router(order_item_router)
-api_router.include_router(restaurant_times_router)
+api_router.include_router(login_router)
+api_router.include_router(register_router)
