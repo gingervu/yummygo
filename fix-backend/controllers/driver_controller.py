75cb6d85e953c1edcf716f84fb0884b631dd94d2
anh7777 <nguyenngocanh_t67@hus.edu.vn>
2025-01-17 20:29:05 +0700

    add restaurant

---
+++ fix-backend/controllers/driver_controller.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from db.database import get_db
+from models.schemas import *
+from models.models import *
+from services.driver_service import *
+
+from middlewares.auth_middleware import get_current_user, require_role
+
+router = APIRouter(prefix="/drivers", tags=["Drivers"])
+
+# ------------------------------
+# Quản lý Tài Xế (Controller)
+# ------------------------------
+
+# Lấy ra thông tin tài xế
+@router.get("/me", response_model=DriverResponse)
+async def get_driver(current_driver: dict = Depends(require_role("driver")), db: Session = Depends(get_db)):
+    return get_driver_service(current_driver['user_id'], db)
+
+
+# Chỉnh sửa thông tin tài xế
+@router.put("/update", response_model=DriverResponse)
+async def update_driver(driver: DriverCreate, current_driver: dict = Depends(require_role('driver')), db: Session = Depends(get_db)):
+    return update_driver_service(driver, current_driver['user_id'], db)
+
+# Chuyển trạng thái tài xế
+@router.put("/change-status")
+async def update_status(current_driver: dict = Depends(require_role('driver')), db: Session = Depends(get_db)):
+    driver = update_driver_status(current_driver['user_id'], db)
+    return {"message" : "Status changed", "status" : driver.status}
+
+
+# Xóa tài xế
+@router.delete("/delete")
+async def delete_driver(current_driver: dict = Depends(require_role("driver")), db: Session = Depends(get_db)):
+    return delete_driver_service(current_driver['user_id'], db)
+
+# Lấy ra thông tin của tài xế để hiện cho khách hàng và nhà hàng xem
+@router.get("/info/{driver_id}", response_model=DriverResponse)
+async def driver_info(driver_id: int, db: Session = Depends(get_db)):
+    return db.query(Driver).filter(Driver.driver_id == driver_id)
+
+# Lấy ra danh sách mã đơn hàng hiện tại của tài xế
+# Dùng mã đơn để hiển thị thông tin của đơn hàng khi tài xế
+# chọn xem đơn
+@router.get("/order")
+async def driver_order(current_driver: dict = Depends(require_role('driver')), db: Session = Depends(get_db)):
+    return get_current_driver_order(current_driver['user_id'], db)
+
+# lấy ra danh sách tất cả đơn hàng của tài xế
+@router.get("/all-orders")
+async def driver_orders(current_driver: dict = Depends(require_role('driver')), db: Session = Depends(get_db)):
+    return get_driver_orders(current_driver['user_id'], db)
---
+++ fix-backend/controllers/login_controller.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends, HTTPException, Response
+from sqlalchemy.orm import Session
+from typing import List
+from db.database import get_db
+from models.schemas import *
+from fastapi.responses import JSONResponse
+from services.login_service import authenticate_login
+from utils.access_token import create_access_token
+from datetime import timedelta
+from fastapi import FastAPI, Depends, HTTPException
+
+router = APIRouter()
+
+# endpoint đăng nhập
+@router.post("/token")
+async def login(user: UserLogin, db: Session = Depends(get_db)):
+    user_id, role = authenticate_login(user, db)
+    if not user_id:
+        raise HTTPException(status_code=401, detail="Thông tin đăng nhập không hợp lệ")
+    access_token = create_access_token({"user_id": user_id, "role": role})
+    
+    # response.set_cookie(
+    #     key="access_token",
+    #     value=access_token,
+    #     path="/",
+    #     httponly=False,
+    #     secure=False,  
+    #     max_age=timedelta(days=3),
+    #     expires=timedelta(days=3),
+    #     domain=".localhost",
+    #     samesite="none" 
+    # )
+    
+    return {"access_token": access_token}
